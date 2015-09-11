3bc737fc798f7d70d5a5bf68913a70699c20b75a
Cedric Villa <cedric.villa@gmail.com>
2015-09-11 07:51:38 +0200

    add proxy to sampler

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM node
 RUN npm install -g hem
 RUN npm install express
+RUN npm install express-http-proxy
 RUN npm install -g spine.app
 RUN mkdir /webui
 COPY src/ /webui/src/
--- webui.js
+++ webui.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var proxy = require('express-http-proxy');
 var app = express();
 
 // app.get('/', function (req, res) {
@@ -10,3 +11,9 @@ app.use(express.static('src/public'));
 var server = app.listen(80, function () {
     console.log('WEBUI running on port 80');
 });
+
+app.use('/sampler', proxy('http://sampler', {
+  forwardPath: function(req, res) {
+    return require('url').parse(req.url).path;
+  }
+}));
